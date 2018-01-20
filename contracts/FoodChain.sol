900fd716be5932108f8550d01386d6880a712795
Aleksas Kekys <aleksas@Aleksass-MacBook-Pro.local>
2018-01-20 20:42:12 +0000

    Adding dynamic cooperatives

--- contracts/FoodChain.sol
+++ contracts/FoodChain.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.4.17;
 contract FoodChain {
     
     struct Field {    
-        string Produce;
+        string Product;
         uint Yield;
+        bool IsFilled;
     }
 
     struct Farmer {
         address Uid;
         string Name;
         string Surname;
-        uint Longitude;
-        uint Lattitude;    
-        mapping (string=>Field) Land;
+        uint8 Longitude;
+        uint8 Lattitude;
+        mapping (string => Field) Land;
     }
 
     struct Buyer {
@@ -27,45 +28,49 @@ contract FoodChain {
         address Buyer;
         string Product;
         uint Quantity;
-        uint Price;        
+        uint Price;
     }
 
-    struct Cooperative {    
-        address Uid;
-        string Product;
-        uint HarvestMonth;
+    struct CooperativeContract {    
+        uint Uid;
+        BuyOrder Order;
         // Farmer to What quantity of string does each farmer put for sale
-        mapping(address => uint) ShareInCooperative;  
+        mapping(address => uint) ShareInCooperative;
     }
 
     uint totalOrders = 0;
-    mapping (address=>Farmer) public Farmers;
+    Farmer[] Farmers;
     mapping (address=>Buyer) public Buyers;
     mapping (address=>uint) BuyerActiveBids;
     BuyOrder[] allOrders;
+    CooperativeContract[] liveContracts;
     
 
     function registerFarmer(address Uid,
         string Name,
         string Surname,
-        uint Longitude,
-        uint Lattitude) public 
+        uint8 Longitude,
+        uint8 Lattitude) public 
     {
-       Farmers[Uid] = Farmer({ 
+       Farmers.push(Farmer({ 
         Uid : Uid, 
        Name : Name, 
        Surname : Surname, 
        Longitude : Longitude, 
        Lattitude : Lattitude
-       });
+       }));
     }
 
-    function registerFarmerField(address farmerUid, string produce, uint yield) public {
-        
-        Farmers[farmerUid].Land[produce] = Field({
-            Produce : produce,
-            Yield : yield
-        });
+    function registerFarmerField(address farmerUid, string Product, uint yield) public {
+        for (uint i = 0; i < Farmers.length; i++) {
+            if (Farmers[i].Uid == farmerUid) {
+                Farmers[i].Land[Product] = Field({
+                    Product : Product,
+                    Yield : yield,
+                    IsFilled : false
+                });
+            }
+        }
     }
 
     function registerBuyer(address Uid,
@@ -79,21 +84,80 @@ contract FoodChain {
         });
     }
 
-    function putAnOrder(string produce, uint quantity, uint price, address buyerId) public returns (uint) {
+    function putAnOrder(string Product, uint quantity, uint price, address buyerId) public returns (uint) {
         uint newOrderId = ++totalOrders;
         var order = BuyOrder({
              Id : newOrderId,
              Buyer : buyerId,
-             Product : produce,
+             Product : Product,
              Quantity : quantity,
              Price : price  
         });
         
-        allOrders.push(order);
-
         BuyerActiveBids[buyerId] = newOrderId;
+        allOrders.push(order);
+        
+        
 
         return newOrderId;
+    }
+
+    function matchFarmersAndBuyers() public {
+        
+        for (uint256 j; j < allOrders.length; j++) {
+            var order = allOrders[j];
+            var orderQuantity = order.Quantity;
+            uint256 filledQuantity = 0;
+            //todo create a cooperative contract
+            var coopContract = CooperativeContract({
+                                    Uid : order.Id,
+                                    Order : order});
+
+            for (uint256 i; i < Farmers.length ; i++) {
+                var farmer = Farmers[i];
+                var field = farmer.Land[order.Product];
+                if (field.Yield == 0 || field.IsFilled) 
+                    continue;
+                
+                 var pendingQuantity = orderQuantity - filledQuantity;
+
+                if (pendingQuantity >= 0) {
+
+                    var quantityToFill = min(pendingQuantity, field.Yield);
+                   
+                    filledQuantity += quantityToFill;
+
+                    field.Yield -= quantityToFill;
+
+                    var x = coopContract.ShareInCooperative;
+                    x[farmer.Id] = quantityToFill;
+
+                    //todo add farmer to cooperative contract
+
+                    if (field.Yield == 0)
+                        field.IsFilled = true;
+                }
+                coopContract = coopContract;
+                if (pendingQuantity == 0) 
+                    break;
+            }
+
+            liveContracts.push(coopContract);
+        }
+    }
+
+    function max(uint a, uint b) public returns (uint) {
+        if (a > b) 
+            return a;
+        else 
+            return b;
+    }
+
+    function min(uint a, uint b) public returns (uint) {
+        if (a < b) 
+            return a;
+        else 
+            return b;
     }
 
 }
